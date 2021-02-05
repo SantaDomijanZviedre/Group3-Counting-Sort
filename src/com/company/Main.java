2e04fae57ff0e856a4195f0c14513469e47c7b5c
Santa Domijana-Zviedre <balticadriatic@gmail.com>
2021-02-05 01:04:48 +0100

    BubbleSort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -9,8 +11,40 @@ public class Main {
         Random rd = new Random();
         int[] myArray = new int[20000];
         for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = rd.nextInt();
-            System.out.println(myArray[i]);
+          myArray[i] = rd.nextInt();
         }
+          //System.out.println(Arrays.toString(myArray));
+
+        {
+            long startTime = System.currentTimeMillis();
+            long counts = bubbleSort(myArray);
+            long endTime = System.currentTimeMillis();
+            System.out.println("Array was sorted in " + (endTime - startTime) + " miliseconds.");
+
+            //System.out.println(Arrays.toString(myArray));
+            System.out.println("There were " + counts + " loops used in the sorting process.");
+        }
+
+    }
+
+
+
+
+    public static long bubbleSort(int[] myArray) {
+        boolean sorted = false;
+        long counter = 0;
+        while (!sorted) {
+            sorted = true;
+            counter++;
+            for (int i = 0; i < myArray.length - counter; i++) {
+                if (myArray[i] > myArray[i + 1]) {
+                    int temp = myArray[i];
+                    myArray[i] = myArray[i + 1];
+                    myArray[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+       return counter;
     }
 }
